a3b9c27c65c875745282ce2053643be62af19c2b
Arno <31947147+Alwaysproblem@users.noreply.github.com>
2017-11-28 19:41:44 +1100

    not finished

--- convert2CNF.py
+++ convert2CNF.py
@@ -13,6 +13,45 @@
 #        and means    interception
 #        or  means    Union
 
+import argparse
 
+def parserArgument():
+    """
+    acquire the system parameters
+    """
+    parser = argparse.ArgumentParser(description = 'Convert2CNF Project')
+    parser.add_argument('proposition', type = str,\
+        help = "the propsition you need to prove,the format is '[p imp q, (neg r) imp (neg q)] seq [p imp r]'",\
+        action = 'store')
+
+    return parser.parse_args()
+
+class CNF(object):
+    def __init__(self, proposition):
+        self.prop = proposition
+
+    def funcname(self, parameter_list):
+        pass
+
+    def is_CNF(self):
+        pass
+
+    def morgan_rules(self, mode):
+        pass
+
+    def gen_tries(self):
+        pass
+
+    def showCNF(self):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    argv = parserArgument()
+    pro_after_CNF = CNF(argv.proposition)
+    # print(argv.proposition)
 
 
